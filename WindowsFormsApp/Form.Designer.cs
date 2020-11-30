8d893d4ae332ea58dc8b35f761401e82e0762fa1
プロちゃん <->
2020-11-30 20:51:48 +0900

    ソースコードを追加

---
+++ WindowsFormsApp/Form.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MessageLabel = new System.Windows.Forms.Label();
+            this.actionButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MessageLabel
+            // 
+            this.MessageLabel.AutoSize = true;
+            this.MessageLabel.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.MessageLabel.Font = new System.Drawing.Font("Meiryo UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.MessageLabel.Location = new System.Drawing.Point(385, 213);
+            this.MessageLabel.Name = "MessageLabel";
+            this.MessageLabel.Size = new System.Drawing.Size(264, 35);
+            this.MessageLabel.TabIndex = 0;
+            this.MessageLabel.Text = "操作内容を表示します";
+            // 
+            // actionButton
+            // 
+            this.actionButton.Font = new System.Drawing.Font("Meiryo UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.actionButton.Location = new System.Drawing.Point(222, 207);
+            this.actionButton.Name = "actionButton";
+            this.actionButton.Size = new System.Drawing.Size(113, 46);
+            this.actionButton.TabIndex = 1;
+            this.actionButton.Text = "Action";
+            this.actionButton.UseVisualStyleBackColor = true;
+            this.actionButton.Click += new System.EventHandler(this.actionButton_Click);
+            this.actionButton.MouseLeave += new System.EventHandler(this.actionButton_MouseLeave);
+            this.actionButton.MouseHover += new System.EventHandler(this.actionButton_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.actionButton);
+            this.Controls.Add(this.MessageLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MessageLabel;
+        private System.Windows.Forms.Button actionButton;
+    }
+}
+
---
+++ WindowsFormsApp/Form.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// クリック時の処理です。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void actionButton_Click(object sender, EventArgs e)
+        {
+            this.MessageLabel.Text = "クリックされました。";
+        }
+
+        /// <summary>
+        /// マウスが乗った時の処理です。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void actionButton_MouseHover(object sender, EventArgs e)
+        {
+            this.MessageLabel.Text = "マウスが乗りました。";
+        }
+
+        /// <summary>
+        /// マウスが外れた時の処理です。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void actionButton_MouseLeave(object sender, EventArgs e)
+        {
+            this.MessageLabel.Text = "マウスが降りました。";
+        }
+    }
+}
